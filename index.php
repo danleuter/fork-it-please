f215c914b1ce9f44a77e2ac5d8a8dc7548ee286d
khalelogram <khal.bangcola@gmail.com>
2019-12-07 14:25:51 +0800

    feature/home: adding h1 tag with home page value

--- index.php
+++ index.php
@@ -10,7 +10,9 @@
   ?>
 
   <!-- Page Content -->
-  <div class="container"></div>
+  <div class="container">
+    <h1>Home Page</h1>
+  </div>
   <!-- /.container -->
 
  <?php include_once("./components/footer.php"); ?>
